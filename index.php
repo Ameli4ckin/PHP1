689cd8abb9d031ec115753b6313712eca1313d39
Ameli4ckin <iameli4ckin@mail.ru>
2022-12-06 14:07:28 +0300

    added fixes

--- index.php
+++ index.php
@@ -7,27 +7,25 @@ $str = "Вас зовут $name, вам $age лет.\n";
 
 echo $str;
 
+const TASK_QUESTION = "Какая задача стоит перед вами сегодня? \n";
+const TASK_TIME_QUESTION = "Сколько примерно времени эта задача займет? \n";
 
-$task1 = readline("Какая задача стоит перед вами сегодня? \n");
-$taskTime1 = readline("сколько примерно времени эта задача займет? \n");
+$task1 = readline(TASK_QUESTION);
+$taskTime1 = (int)readline(TASK_TIME_QUESTION);
 
-$task2 = readline("Какая задача стоит перед вами сегодня? \n");
-$taskTime2 = readline("сколько примерно времени эта задача займет? \n");
+$task2 = readline(TASK_QUESTION);
+$taskTime2 = (int)readline(TASK_TIME_QUESTION);
 
-$task3 = readline("Какая задача стоит перед вами сегодня? \n");
-$taskTime3 = readline("сколько примерно времени эта задача займет? \n");
+$task3 = readline(TASK_QUESTION);
+$taskTime3 = (int)readline(TASK_TIME_QUESTION);
 
 $taskSum = $taskTime1 + $taskTime2 + $taskTime3;
-$taskTime1 .= "ч";
-$taskTime2 .= "ч";
-$taskTime3 .= "ч";
-$taskSum .=  "ч";
 
 echo <<<HERE
 $name, сегодня у вас запланировано 3 приоритетных задачи на день:
-- $task1 ($taskTime1)
-- $task2 ($taskTime2)
-- $task3 ($taskTime2)
-Примерное время выполнения плана = $taskSum
+- $task1 ({$taskTime1}ч)
+- $task2 ({$taskTime2}ч)
+- $task3 ({$taskTime3}ч)
+Примерное время выполнения плана = {$taskSum}ч
 HERE;
 
