4c46f75beace41fad4eb89b82e7244e9d445498b
Ameli4ckin <iameli4ckin@mail.ru>
2023-01-10 12:47:15 +0300

    dz6

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
+$controller = $_GET['controller'] ?? 'index';
+$routes = require 'routes.php';
 
-include "Task.php";
-include "User.php";
-include "Comment.php";
-include "TaskService.php";
-
-$user1 = new User("Денис", "test1@mail.com");
-$task1 = new Task("task 1", $user1);
-$task1->setDescription("task 1 new");
-$task1->markAsDone();
-
-$user2 = new User("Павел", "test2@mail.com");
-TaskService::addComment($user2, $task1, "Комментарий");
-$task1->setDescription("Описание");
+require_once $routes[$controller] ?? die('404');
